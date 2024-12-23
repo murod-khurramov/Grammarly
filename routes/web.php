1d069cca4daa1c6c6c055f02d35490179d5d83d5
Khurramov <murod_khurramov@icloud.com>
2024-12-23 11:16:53 +0500

    Update web routes to include profile routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile');
+});
