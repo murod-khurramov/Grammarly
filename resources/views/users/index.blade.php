a361ff34dc3660488a31ca4b8befbd4198ce088b
Khurramov <murod_khurramov@icloud.com>
2024-12-23 16:18:04 +0500

    Enhance User index view with additional details

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,12 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Barcha foydalanuvchilar</h1>
-    <ul>
-        @foreach ($users as $user)
-            <li>
-                <a href="{{ route('users.show', $user->id) }}">{{ $user->name }}</a>
-            </li>
-        @endforeach
-    </ul>
+    <div class="container">
+        <div id="user-list">
+            @foreach($users as $user)
+                <div class="user-item">
+                    <p>{{ $user->name }}</p>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+    <!-- JavaScript va AJAX orqali qidiruvni sozlash -->
+    <script>
+        document.getElementById('search').addEventListener('input', function() {
+            let searchQuery = this.value;
+            if (searchQuery.length >= 3) {
+                fetch(`/users/search?q=${searchQuery}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        let userList = document.getElementById('user-list');
+                        userList.innerHTML = '';  // Oldingi foydalanuvchilarni tozalash
+
+                        data.forEach(user => {
+                            let userDiv = document.createElement('div');
+                            userDiv.classList.add('user-item');
+                            userDiv.innerHTML = `<p>${user.name}</p>`;
+                            userList.appendChild(userDiv);
+                        });
+                    });
+            }
+        });
+    </script>
 @endsection
